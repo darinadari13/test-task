aaff6c5b14ff7be22e9945d1a116b907713b4c15
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-04-02 15:27:32 +0300

    styles for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import styles from './index.module.scss';
 function Header() {
   return (
     <header className={styles.container}>
-      <div>
-        <span>trppd_</span>
+      <div className={styles.logo}>
+        <h1>trppd_</h1>
       </div>
       <div className={styles.smallContainer}> 
         <span>My Collection</span>
