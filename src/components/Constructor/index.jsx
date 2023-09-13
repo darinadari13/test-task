08f7a7e53b02c3a61d32e7c0e78b3d83dfc0011a
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-09-13 19:51:19 +0300

    dnd update

--- src/components/Constructor/index.jsx
+++ src/components/Constructor/index.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import { getCurrentPageSelector } from "../../redux/slices/app/selectors"
 import { ConstructorElement } from "../ConstructorElement";
-import { Droppable } from "react-beautiful-dnd";
+import { Droppable, Draggable } from "react-beautiful-dnd";
 
 export function Constructor() {
   const page = useSelector(getCurrentPageSelector)
@@ -14,7 +14,17 @@ export function Constructor() {
             className="flex flex-col gap-y-4 bg-slate-200 w-2/4 p-5" ref={provider.innerRef}
             {...provider.droppableProps}
           >
-            {page.map(element => <ConstructorElement key={element.id} element={element} />)}
+            {page.map((element, index) => (
+              <Draggable key={element.id} draggableId={element.id} index={index}>
+                {
+                  provider => (
+                    <div ref={provider.innerRef} {...provider.draggableProps} {...provider.dragHandleProps}>
+                      <ConstructorElement key={element.id} element={element} />
+                    </div>
+                  )
+                }
+              </Draggable>
+            ))}
           </div>
         )
       }
