9418c079894508715a480e88c482620cc05097b0
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-09-13 15:56:05 +0300

    created layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
-      </p>
+    <div className="min-h-screen bg-slate-100">
+      <header className="p-5 border-b">
+        REACT EDITOR Test
+      </header>
+      <main className="flex h-screen">
+        <div className="p-5 w-1/4 border-r">
+          Левый блок
+        </div>
+        <div className="p-5 w-1/2 border-r">
+          Средний блок
+        </div>
+        <div className="p-5 w-1/2">
+          Правый блок
+        </div>
+      </main>
     </div>
   );
 }
