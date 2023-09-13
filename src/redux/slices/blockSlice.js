2002fbf590467a046414531af71e7a0484b9b254
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-09-13 14:55:33 +0300

    instelled beautiful dnd

--- src/redux/slices/blockSlice.js
+++ src/redux/slices/blockSlice.js
@@ -6,7 +6,7 @@ const initialState = {
     { id: generateUniqueId(), type: 'headline', text: 'A legendary cap set that feels like new' },
     { id: generateUniqueId(), type: 'paragraph', text: 'Prompt your customer to revisit your store by showing them the products they left behind. Consider offering them a coupon code to close the deal. Using the "Insert" panel on the right, drag and drop the Abandoned Cart element onto the page below.' },
     { id: generateUniqueId(), type: 'button', text: 'Register now' },
-    { id: generateUniqueId(), type: 'image', text: '' },
+    { id: generateUniqueId(), type: 'image', text: '../../../public/images/defaultImg.png' },
   ],
 };
 
