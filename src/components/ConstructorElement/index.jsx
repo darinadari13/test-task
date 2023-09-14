904c2e674c6c9cf4a9a1888b2e74604f5e49e6d7
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-09-14 19:45:54 +0300

    input styles

--- src/components/ConstructorElement/index.jsx
+++ src/components/ConstructorElement/index.jsx
@@ -2,7 +2,7 @@ import { useCallback } from "react"
 import { ELEMENT_ICON_BY_TYPE, ELEMENT_NAME_BY_TYPE } from "../../constants"
 import cn from 'classnames'
 
-const INPUT_CLASS = "bg-white border rounded-sm w-full border-4 border-solid border-transparent box-shadow-inset mt-2.5 p-5 shadow-custom font-roboto text-xs"
+const INPUT_CLASS = "w-full font-roboto text-xs rounded-sm border p-5"
 
 export function ConstructorElement({ index, element, isSelected, onClick, onChange }) {
   const IconComponent = ELEMENT_ICON_BY_TYPE[element.type]
@@ -17,13 +17,15 @@ export function ConstructorElement({ index, element, isSelected, onClick, onChan
   }, [index, onChange])
 
   return (
-    <div className={cn('flex', 'flex-col', 'items-center', 'rounded-lg', 'p-4', {
+    <div className={cn('flex', 'flex-col', 'items-center', 'rounded-lg', 'p-4','cursor-pointer', {
       'bg-slate-100': !isSelected,
       'bg-custom-grey': isSelected,
     })} onClick={handleClick}>
       <IconComponent  className="mb-2.5"/>
-      <span className="font-roboto text-xs">{elementName}</span>
-      {isSelected && <input type="text" className={INPUT_CLASS} defaultValue={element.data} onChange={handleChange} />}
+      <span className="font-roboto text-xs mb-5">{elementName}</span>
+      {isSelected && <div className="w-full p-5 bg-white flex rounded-sm border-custom-light-grey">
+        <input type="text" className={INPUT_CLASS} defaultValue={element.data} onChange={handleChange} />
+      </div>}
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,8 @@ module.exports = {
         'custom-dark': '#252A32',
         'custom-blue-grey': '#97AACD',
         'custom-blue': '#4368E0',
-        'custom-white': '#F6F9FE'
+        'custom-white': '#F6F9FE',
+        'custom-light-grey': '#E4E6F1'
       },
       boxShadow: {
         'custom': '0px 2px 2px 0px rgba(211, 214, 215, 0.20), 0px 4px 4px 0px rgba(211, 214, 215, 0.20), 0px 16px 16px 0px rgba(211, 214, 215, 0.20), 0px 32px 32px 0px rgba(211, 214, 215, 0.20), 0px 64px 64px 0px rgba(211, 214, 215, 0.20)',
@@ -23,15 +24,6 @@ module.exports = {
     },
 
   },
-  plugins: [
-    function ({ addUtilities }) {
-      const newUtilities = {
-        '.box-shadow-inset': {
-          'box-shadow': 'inset 0 0 0 1px #E4E6F1',
-        },
-      };
-      addUtilities(newUtilities);
-    },
-  ],
+  plugins: [],
 }
 
