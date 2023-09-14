d33b532a023338bceef9451bb5570f801f0ff087
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-09-14 20:05:47 +0300

    update

--- src/components/ConstructorElement/index.jsx
+++ src/components/ConstructorElement/index.jsx
@@ -1,6 +1,7 @@
 import { useCallback } from "react"
 import { ELEMENT_ICON_BY_TYPE, ELEMENT_NAME_BY_TYPE } from "../../constants"
 import cn from 'classnames'
+import { ReactComponent as ImageButtonIcon } from '../../assets/icons/imageButton.svg';
 
 const INPUT_CLASS = "w-full font-roboto text-xs rounded-sm border p-5"
 
@@ -17,15 +18,28 @@ export function ConstructorElement({ index, element, isSelected, onClick, onChan
   }, [index, onChange])
 
   return (
-    <div className={cn('flex', 'flex-col', 'items-center', 'rounded-lg', 'p-4','cursor-pointer', {
+    <div className={cn('flex', 'flex-col', 'items-center', 'rounded-lg', 'p-4','cursor-pointer', 'relative', {
       'bg-slate-100': !isSelected,
       'bg-custom-grey': isSelected,
     })} onClick={handleClick}>
-      <IconComponent  className="mb-2.5"/>
+      <IconComponent className="mb-2.5"/>
       <span className="font-roboto text-xs mb-5">{elementName}</span>
-      {isSelected && <div className="w-full p-5 bg-white flex rounded-sm border-custom-light-grey">
-        <input type="text" className={INPUT_CLASS} defaultValue={element.data} onChange={handleChange} />
-      </div>}
+
+      { isSelected &&
+        <div className="w-full p-5 bg-white flex rounded-sm border-custom-light-grey">
+          <input type="text" className={INPUT_CLASS} defaultValue={element.data} onChange={handleChange} />
+        </div>
+      }
+      <div className="absolute -top-6 right-2 flex">
+        <div className="flex rounded-t bg-blue-200 mr-5">
+          <button><ImageButtonIcon /></button>
+          <button><ImageButtonIcon /></button>
+        </div>
+        <div className="flex rounded-t bg-blue-300">
+          <button><ImageButtonIcon /></button>
+          <button><ImageButtonIcon /></button>
+        </div>
+      </div>
     </div>
   )
 }
